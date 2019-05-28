69c619190904029f1dc13fda9859dc7d30f8ef44
Flor <florh89@hotmail.com>
2019-05-27 22:06:00 -0300

    Agrego los archivos de la clase EVENTOS

--- script.js
+++ script.js
@@ -1,106 +1,51 @@
+let button = document.querySelector("button");
+let body = document.querySelector("body");
+let contador = 1
+//Evento: la ejecucion de una funcion (call back) como respuesta a una accion
+//LA ACCION A LA QUE RESPONDEMOS CON EL BOTON ES EL CLICK
+
+//Las funciones en este lenguaje son objetos
+// se podria haber escrito 
+// var hacerClick = function(){}
+// TAMBIEN ES LO MISMO QUE HACER window.hacerClick = function(){}
+
+ //no ejecuto la funcion solo la nombros
 /**
- * Primitivos
- * 
- * String: cadena de texto entre comillas dobles o simples
- * Nbr: no se distingue entre tipos de numeros
- * Boolean
- * Undefined: una variable sin valor
- * Null
-
-    objetos:
-
-    1) object: se crean con llaves {}
-    2) array: se crean con corchetes []
-    3) function
-
- * 
-
-    Variables: 
-    var - let - const
-
-    Los nombres de variables pueden arrancar con _ $ o cualquier letra, despues del primer caracter
-    puede ir cualquier otro caracter
-
-    Console -> object - nos da funciones de logueo en consola de desarrollo
-
-    location -> object 
-
-    history
+ * Reemplazar el codigo de la funcion hacerClick por:
+ * 1) Crear un boton por cada click que se haga
+ * 2)Cada boton nuevo se tiene que ver en el DOM
+ * 3)Cada boton nuevo tiene que tener un callback en su propio evento click tambien
+ * 4)odos los botones tienen que tener como texto "Boton N" donde N es un numero arrancando de uno
+ * el cual se va aumentando de a una unidad
  */
+function nuevoBoton() {
+      let btn2 = document.createElement("button");
+      btn2.innerText=("boton" + contador++); 
+      body.appendChild(btn2);
+      btn2.addEventListener("click", unamismaFuncionParaTodosLosBotones) 
+   }
+   button.addEventListener("click", nuevoBoton)
 
- let nombre = "Florencia"
- let apellido = "Hollmann"
- let nombre_completo = nombre + " " + apellido
- console.log(nombre_completo)
+   function unamismaFuncionParaTodosLosBotones () {
+      console.log ("Click de alguno de los botones")
+   }
 
- let numeros = [10,7,5,2,0, "Flor", true]
- numeros.push("hola")
- numeros.push(45)
+   /*
+      Practicar en un HTML nuevo:
+      1) Hacer una etiqueta button con un texto que diga "Crear parrafo:"
 
- console.log(numeros)
- console.log(numeros.length)
- console.log(numeros["length"])
- 
- console.table(numeros)
+      2) En JS agarrar nodo 
+      3) Registrarle un callbac a su click
+      4) El callback del boton tiene que poder crear la siguiente estructura: }
 
- let persona= {
-     nombre: "Horacio",
-     edad: 29,
-     vive: true
- }
+      <div>
+         <p>Lorem Ipsum</p>
+         <buttton>Borrar</button>
+      </div>
+      5) El boton que tiene el texto borrar ambien tiene que hacer un click y su callback tiene que
+      poder borrar el P de arriba al cual pertenece usando la funcion:
 
- persona.trabajo = false
- persona.tienePerrito = 3
+      NodoT.removeChild(NodoC) : esta funcion le reueve el NodoC al nodoT
 
- for (let i = 0; i < numeros.length; i++) {
-     console.log(numeros[i]);
-     
- }
+   */
 
- for (prop in persona) {
-    console.log(persona)
-    
- }  
-
- console.clear()
- let dos= document.getElementById("item2")
- console.dir(dos)
- //hago console DIR y no console LOG asi puedo ver la variable 2 como objeto
- //si hago console log veo la etiquiet de html!
- let lis = document.getElementsByTagName("li")
- let tres = document.querySelector("#item2")
-
- //como creo un NODO
- let p = document.createElement("p")
- p.innerText = "Mi nuevo texto"
- p.id = "nuevo"
- p.className = "item"
-
- console.log(p)
-
- //agregar nodos al DOM 
- // Desde un nodo cualquiera que yo ya tenga seleccionado
- // nodo.appendChild(NodoNuevo) -> esto inserta el nodo nuevo como ultimo hijo de nodo
-//
-let body = document.querySelector("body")
-body.appendChild(p)
- // Nodo.insertBefore(NodoNuevo, NodoRef)
- //
-
- let li2 = document.createElement("li")
- li2.innerText = "soy el nuevo primer elemento!"
-
- let ul = document.querySelector("ul")
- let li3 = document.querySelector("#item1")
- ul.insertBefore(li2, li3)
-
- let NuevoUl = document.createElement("ul")
- for (let index = 0; index < 5; index++) {
-    
-   let liNUEVO = document.createElement("li")
-   NuevoUl.appendChild(li)
-   liNUEVO.innerText = "Nueo texto" + i
-    
- }
-
- body.appendChild(NuevoUl)
